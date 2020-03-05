b2a2941de1f37bdcb80fb143588751b7f258979a
jharte5 <jonathan.harte@codeimmersives.com>
2020-03-05 10:16:23 -0500

    fixing last issue

--- oshop/routes/index.js
+++ oshop/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const Cards = require('./users/models/Cards.json')
 /* render home page. */
 router.get('/', (req, res, next) => {
-  return res.render('main/home');
+  let Cards = [{img, title, text}]
+  return res.render('main/home',{title: 'Online shopper', Cards});
 });
 
 router.get('/logout', (req,res) => {
